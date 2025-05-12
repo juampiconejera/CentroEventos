c146e4817dd9a44c3024822313001bc3b96fc962
Matibrizzi23 <matias.brizzi181440@alumnos.info.unlp.edu.ar>
2025-05-12 17:43:27 -0300

    Add IRepositorioEvento

---
+++ CentroEventos.Aplicacion/Interfaces/IRepositorioEventoDeportivo.cs
@@ -0,0 +1,13 @@
+namespace   CentroEventos.Aplicacion.Interfaces;
+using CentroEventos.Aplicacion.Entidades;
+
+public interface IRepositorioEventoDeportivo
+{
+    //Deben manejar las operaciones CRUD básicas necesarias para
+    //los casos de uso y asegurar que los datos persistan entre ejecuciones del programa
+    void Agregar (EventoDeportivo eventoDeportivo);
+    void Eliminar (int id);
+    void Modificar (EventoDeportivo eventoDeportivo);
+    EventoDeportivo ObtenerPorId (int id);
+    List<EventoDeportivo> Listar ();
+}
