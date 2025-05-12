c75024f7ba558d68c4b7a511d9e24095b0ca940e
Matibrizzi23 <matias.brizzi181440@alumnos.info.unlp.edu.ar>
2025-05-12 00:46:55 -0300

    Update IRepositorioReserva.cs

---
+++ CentroEventos.Aplicacion/Interfaces/IRepositorioReserva.cs
@@ -0,0 +1,12 @@
+namespace CentroEventos.Aplicacion.Interfaces;
+using CentroEventos.Aplicacion.Entidades;
+    public interface IRepositorioReserva
+    {
+        void Agregar(Reserva reserva);
+        void Eliminar(int id);
+        void Modificar(Reserva reserva);
+        Reserva ObtenerPorId(int id);
+        List<Reserva> Listar();
+        
+        
+    }
