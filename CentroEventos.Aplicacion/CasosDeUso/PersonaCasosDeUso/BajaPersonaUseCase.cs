63892eede14334b9e920987dffd1a99c23bba9e9
JuampiConejera <juampiconejera@gmail.com>
2025-05-16 12:19:35 -0300

    Change IRepoPersona ( void Eliminar(Persona persona) to void Eliminar(int id) ) Solamente encontrando el id podemos encontrar cualquier persona
    Add BajaPersonaUseCase

--- CentroEventos.Aplicacion/CasosDeUso/PersonaCasosDeUso/BajaPersonaUseCase.cs
+++ CentroEventos.Aplicacion/CasosDeUso/PersonaCasosDeUso/BajaPersonaUseCase.cs
@@ -1,10 +1,19 @@
 using System;
 using System.Net.Security;
+using CentroEventos.Aplicacion.Entidades;
+using CentroEventos.Aplicacion.Excepciones;
 using CentroEventos.Aplicacion.Interfaces;
 namespace CentroEventos.Aplicacion.CasosDeUso.PersonaCasosDeUso;
 
-public class BajaPersonaUseCase(IRepositorioPersona repositorioPersona /*, IServicioAutorizacionProvisorio Auth*/)
+public class BajaPersonaUseCase(IRepositorioPersona repoPersona, IServicioAutorizacionProvisorio Auth)
 {
-    public void Ejecutar()
-    /*if(!Auth.EstaAutorizado()*/
+    public void Ejecutar(Persona persona, int idUsuario)
+    {
+        if (!Auth.PoseeElPermiso(idUsuario))
+        {
+            throw new FalloAutorizacionException("Usuario no autorizado.");
+        }
+
+        repoPersona.Eliminar(persona.Id);
+    }
 }
--- CentroEventos.Aplicacion/Interfaces/ IRepositorioPersona.cs
+++ CentroEventos.Aplicacion/Interfaces/ IRepositorioPersona.cs
@@ -3,7 +3,7 @@ using CentroEventos.Aplicacion.Entidades;
 public interface IRepositorioPersona
 {
     void Agregar(Persona persona);
-    void Eliminar(Persona persona);
+    void Eliminar(int id);
     void Modificar(Persona persona);
     bool ExistePorId(int id);
     bool ExistePorDni(string dni);
