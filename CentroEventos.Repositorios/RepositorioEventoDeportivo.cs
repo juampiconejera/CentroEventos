ece810088eb939a15148cab2b03b33f77c2c7f0c
franndogil <franndogil@gmail.com>
2025-06-17 20:59:12 -0300

    RepEventoDeportivo NO FINALIZADO

--- CentroEventos.Repositorios/RepositorioEventoDeportivo.cs
+++ CentroEventos.Repositorios/RepositorioEventoDeportivo.cs
@@ -11,112 +11,70 @@ namespace CentroEventos.Repositorios;
 
 public class RepositorioEventoDeportivo : IRepositorioEventoDeportivo
 {
-    readonly string _nombreArchivo = "eventos.txt";
-    private readonly IRepositorioReserva _repoReserva;
-    private readonly IRepositorioPersona _repoPersona;
-
-    public RepositorioEventoDeportivo(IRepositorioReserva repoReserva, IRepositorioPersona repoPersona)
-    {
-        _repoReserva = repoReserva;
-        _repoPersona = repoPersona;
-    }
-    private int GenerarId()
-    {
-        return Listar().Count()+1;
-    }
     public void Agregar(EventoDeportivo eventoDeportivo)
     {
-        eventoDeportivo.Id = GenerarId();
-        using var sw = new StreamWriter(_nombreArchivo, true);
-        sw.WriteLine(eventoDeportivo.Id);
-        sw.WriteLine(eventoDeportivo.Nombre);
-        sw.WriteLine(eventoDeportivo.Descripcion);
-        sw.WriteLine(eventoDeportivo.FechaHoraInicio);
-        sw.WriteLine(eventoDeportivo.DuracionHoras);
-        sw.WriteLine(eventoDeportivo.CupoMaximo);
-        sw.WriteLine(eventoDeportivo.ResponsableId);
+        using (var context = new CentroEventosContext())
+        {
+            context.Add(eventoDeportivo);
+            context.SaveChanges();
+        }
     }
 
     public void Modificar(EventoDeportivo eventoModif)
     {
-        var listaTotal = Listar();
-        for (int i = 0; i < listaTotal.Count; i++)
+        using (var context = new CentroEventosContext())
         {
-            if (listaTotal[i].Id == eventoModif.Id)
+            EventoDeportivo? eventoDeportivo = context.EventosDeportivos.FirstOrDefault(e => e.Id == eventoModif.Id);
+            if (eventoDeportivo != null)
             {
-                listaTotal[i] = eventoModif;
-                break;
+                eventoDeportivo.Nombre = eventoModif.Nombre;
+                eventoDeportivo.Descripcion = eventoModif.Descripcion;
+                eventoDeportivo.FechaHoraInicio = eventoModif.FechaHoraInicio;
+                eventoDeportivo.DuracionHoras = eventoModif.DuracionHoras;
+                eventoDeportivo.CupoMaximo = eventoModif.CupoMaximo;
+                eventoDeportivo.ResponsableId = eventoModif.ResponsableId;
+                context.SaveChanges();
             }
-        }
-
-        using var sw = new StreamWriter(_nombreArchivo, false);
-        foreach (EventoDeportivo e in listaTotal)
-        {
-            sw.WriteLine(e.Id); sw.WriteLine(e.Nombre); sw.WriteLine(e.Descripcion); sw.WriteLine(e.FechaHoraInicio); sw.WriteLine(e.DuracionHoras); sw.WriteLine(e.CupoMaximo); sw.WriteLine(e.ResponsableId);
         }
     }
 
     public void Eliminar(int idEvento)
     {
-        var listaTotal = Listar();  //Voy a realizar una baja logica
-        foreach (EventoDeportivo e in listaTotal)
+        using (var context = new CentroEventosContext())
         {
-            if (e.Id == idEvento)
+            EventoDeportivo? eventoDeportivo = context.EventosDeportivos.FirstOrDefault(e => e.Id == idEvento);
+            if (eventoDeportivo != null)
             {
-                e.Nombre = "ELIMINADO";
+                context.Remove(eventoDeportivo);
+                context.SaveChanges();
             }
-        }
-
-        using var sw = new StreamWriter(_nombreArchivo, false);
-        foreach(EventoDeportivo e in listaTotal)
-        {
-            sw.WriteLine(e.Id); sw.WriteLine(e.Nombre); sw.WriteLine(e.Descripcion); sw.WriteLine(e.FechaHoraInicio); sw.WriteLine(e.DuracionHoras); sw.WriteLine(e.CupoMaximo); sw.WriteLine(e.ResponsableId);
         }
     }
 
-    public EventoDeportivo ObtenerPorId(int idEvento)
+    public EventoDeportivo? ObtenerPorId(int idEvento)      //REVISAR LOGICA
     {
-        var listaTotal = Listar();
-        EventoDeportivo eventoRetorno = new EventoDeportivo();
-        foreach (EventoDeportivo e in listaTotal)
+        using (var context = new CentroEventosContext())
         {
-            if (e.Id == idEvento)
-            {
-                eventoRetorno = e;
-                break;
-            }
+            EventoDeportivo? eventoDeportivo = context.EventosDeportivos.FirstOrDefault(e => e.Id == idEvento);
+            return eventoDeportivo;
         }
-        return eventoRetorno;
     }
 
     public bool ExistePorId(int idEvento)
     {
-        var listaTotal = Listar();
-        foreach (EventoDeportivo e in listaTotal)
+        using (var context = new CentroEventosContext())
         {
-            if (e.Id == idEvento)
-            {
-                return true;
-            }
+            EventoDeportivo? evento = context.EventosDeportivos.FirstOrDefault(e => e.Id == idEvento);
+            return evento != null;
         }
-        return false;
     }
 
     public List<EventoDeportivo> Listar()
     {
-        var listaTotal = new List<EventoDeportivo>();
-        using var sr = new StreamReader(_nombreArchivo);
-        while (!sr.EndOfStream)
+        using (var context = new CentroEventosContext())
         {
-            var evento = new EventoDeportivo();
-            evento.Id = int.Parse(sr.ReadLine() ?? ""); evento.Nombre = sr.ReadLine(); evento.Descripcion = sr.ReadLine(); evento.FechaHoraInicio = DateTime.Parse(sr.ReadLine() ?? ""); evento.DuracionHoras = double.Parse(sr.ReadLine() ?? ""); evento.CupoMaximo = int.Parse(sr.ReadLine() ?? ""); evento.ResponsableId = int.Parse(sr.ReadLine() ?? "");
-            if (evento.Nombre != "ELIMINADO")
-            {
-                listaTotal.Add(evento);
-            }
+            return context.EventosDeportivos.ToList();
         }
-
-        return listaTotal;
     }
 
     public List<Persona> ListarPresentes(int idEvento)
@@ -126,7 +84,6 @@ public class RepositorioEventoDeportivo : IRepositorioEventoDeportivo
         var evento = ObtenerPorId(idEvento);
 
         var listaReservas = _repoReserva.Listar();
-        var listaPersonas = _repoPersona.Listar();
 
         foreach (Reserva r in listaReservas)
         {
