b41e653c4550651f52c05297869f43320979a88f
franndogil <franndogil@gmail.com>
2025-06-18 12:17:01 -0300

    add idUsuario, starting RepoUsuarios

--- CentroEventos.Aplicacion/Entidades/Usuario.cs
+++ CentroEventos.Aplicacion/Entidades/Usuario.cs
@@ -8,6 +8,7 @@ namespace CentroEventos.Aplicacion.Entidades;
 public class Usuario
 {
     // Propiedades autoimplementadas
+    public int Id { get; set; }
     public string Nombre { get; set; } = string.Empty;
     public string Apellido { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
