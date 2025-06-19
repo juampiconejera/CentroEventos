7633af08276882fe29c9f13f35e76a22d5a5f99b
franndogil <franndogil@gmail.com>
2025-06-18 22:47:20 -0300

    Add Auth Persona

--- CentroEventos.Aplicacion/CasosDeUso/PersonaCasosDeUso/BajaPersonaUseCase.cs
+++ CentroEventos.Aplicacion/CasosDeUso/PersonaCasosDeUso/BajaPersonaUseCase.cs
@@ -3,16 +3,17 @@ using System.Net.Security;
 using CentroEventos.Aplicacion.Entidades;
 using CentroEventos.Aplicacion.Excepciones;
 using CentroEventos.Aplicacion.Interfaces;
+using CentroEventos.Aplicacion.Enumerativos;
 namespace CentroEventos.Aplicacion.CasosDeUso.PersonaCasosDeUso;
 
 public class BajaPersonaUseCase(IRepositorioPersona repoPersona, IServicioAutorizacion Auth)
 {
-    public void Ejecutar(int id, int idUsuario)
+    public void Ejecutar(int id, Usuario usuario)
     {
-        /* if (!Auth.PoseeElPermiso(idUsuario))
+        if (!Auth.PoseeElPermiso(usuario.Permisos, Permiso.PersonaBaja))
         {
             throw new FalloAutorizacionException("Usuario no autorizado.");
-        } */
+        }
         if (!repoPersona.ExistePorId(id))
         {
             throw new EntidadNotFoundException("El usuario no existe en la base de datos.");
--- CentroEventos.Aplicacion/CasosDeUso/PersonaCasosDeUso/ModificarPersonaUseCase.cs
+++ CentroEventos.Aplicacion/CasosDeUso/PersonaCasosDeUso/ModificarPersonaUseCase.cs
@@ -3,18 +3,18 @@ using CentroEventos.Aplicacion.Entidades;
 using CentroEventos.Aplicacion.Interfaces;
 using CentroEventos.Aplicacion.Excepciones;
 using CentroEventos.Aplicacion.Validadores;
-
+using CentroEventos.Aplicacion.Enumerativos;
 namespace CentroEventos.Aplicacion.CasosDeUso.PersonaCasosDeUso;
 
 public class ModificacionPersonaUseCase(IRepositorioPersona repoPersona, IServicioAutorizacion Auth, PersonaValidador personaValidador)
 {
-    public void Ejecutar(Persona persona, int idUsuario)
+    public void Ejecutar(Persona persona, Usuario usuario)
     {
        //Verificamos los permisos del usuario
-        /* if (!Auth.PoseeElPermiso(idUsuario))
+        if (!Auth.PoseeElPermiso(usuario.Permisos, Permiso.PersonaModificacion))
         {
             throw new FalloAutorizacionException("Usuario no autorizado.");
-        } */
+        }
         //Validamos los datos de la persona
         if(!personaValidador.Validar(persona, out string mensajeError))
         {
